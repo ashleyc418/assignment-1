93f2cb1096f3a8a1c2b8fc61865c0a4e893cefb5
ashleyc418 <159084109+ashleyc418@users.noreply.github.com>
2025-08-26 17:46:14 -0400

    Fix untyped number value

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,16 +1,17 @@
 // Endpoint for querying the fibonacci numbers
 
+import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req, res) => {
-  const { num } = req.params;
+export default (req: Request, res: Response) => {
+  const { num } = req.params.num; // error Unsafe assignment of an `any` value, Unsafe member access .params on an `any` value
 
-  const fibN = fibonacci(parseInt(num));
-  let result = `fibonacci(${num}) is ${fibN}`;
+  const fibN = fibonacci(parseInt(num, 10)); // error Unsafe argument of type `any` assigned to a parameter of type `string`
+  let result = `fibonacci(${num}) is ${fibN}`; // error Invalid type "any" of template literal expression
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`;
+    result = `fibonacci(${num}) is undefined`; // error Invalid type "any" of template literal expression
   }
 
-  res.send(result);
+  res.send(result); // error Unsafe member access .send on an `any` value, Unsafe call of an `any` typed value
 };
