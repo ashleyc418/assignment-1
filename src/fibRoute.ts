c173a6c2a19980233301beaffb5f2659cd924a66
ashleyc418 <159084109+ashleyc418@users.noreply.github.com>
2025-08-26 18:04:02 -0400

    Undo commit

--- src/fibRoute.ts
+++ src/fibRoute.ts
@@ -1,17 +1,16 @@
 // Endpoint for querying the fibonacci numbers
 
-import { Request, Response } from "express";
 import fibonacci from "./fib";
 
-export default (req: Request, res: Response) => {
-  const { num } = req.params.num; // error Unsafe assignment of an `any` value, Unsafe member access .params on an `any` value
+export default (req, res) => {
+  const { num } = req.params;
 
-  const fibN = fibonacci(parseInt(num, 10)); // error Unsafe argument of type `any` assigned to a parameter of type `string`
-  let result = `fibonacci(${num}) is ${fibN}`; // error Invalid type "any" of template literal expression
+  const fibN = fibonacci(parseInt(num));
+  let result = `fibonacci(${num}) is ${fibN}`;
 
   if (fibN < 0) {
-    result = `fibonacci(${num}) is undefined`; // error Invalid type "any" of template literal expression
+    result = `fibonacci(${num}) is undefined`;
   }
 
-  res.send(result); // error Unsafe member access .send on an `any` value, Unsafe call of an `any` typed value
+  res.send(result);
 };
